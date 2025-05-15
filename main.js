64e71b275e8cf86ae0a216f6326327f71473b6a4
lynn89-sudo <luke87joseph@gmail.com>
2025-05-15 12:12:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,11 @@ document.getElementById("insertTOP").innerHTML = `
 <div id="header"></div>
 <div id="navbar"></div>
 `
+
 document.getElementById("navbar").innerHTML = `
 <div id="header-links">
     <a href="index.html"><div style="padding-left:10px; padding-right:10px">Home</div></a>
     <a href="about.html"><div style="padding-left:10px; padding-right:10px">About</div></a>
-    <div style="padding-left:10px; padding-right:10px">News & Legislation</div>
-    <div style="padding-left:10px; padding-right:10px">Parliament</div>
-    <div style="padding-left:10px; padding-right:10px">Committees & Divisions</div>
-    <div style="padding-left:10px; padding-right:10px">Voting</div>
 </div>
 `;
 document.getElementById("header").innerHTML = `
@@ -23,3 +20,10 @@ document.getElementById("logo").onclick = function() {
 document.getElementById("disclaimer").innerHTML = `
     <h3>The Parliament of Hack Club and other associated entities are not associated with the Hack Club non-profit organization
 `
+
+/*
+<div style="padding-left:10px; padding-right:10px">News & Legislation</div>
+<div style="padding-left:10px; padding-right:10px">Parliament</div>
+<div style="padding-left:10px; padding-right:10px">Committees & Divisions</div>
+<div style="padding-left:10px; padding-right:10px">Voting</div>
+*/
