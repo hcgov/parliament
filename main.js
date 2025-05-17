84197bcd68ce247e51a15cd7f2281c919e36a5af
astra-the-boop <astra.the.boop@icloud.com>
2025-05-17 20:59:00 +0700

    FINALLY FUCKING FINISHED

--- main.js
+++ main.js
@@ -36,6 +36,31 @@ setInterval(() => {
     }
 }, 1000);
 
+//tabs
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
 //google sheets api my goat (i hate you)
 let parliamentData;
 
@@ -170,6 +195,136 @@ loadParliamentData(() => {
 
 // render init block
     new Chart(document.getElementById('chart2'), config);
+
+// Instantly assign Chart.js version
+    const chartVersion = document.getElementById('chartVersion');
+    chartVersion.innerText = Chart.version;
+});
+
+let pmData;
+function loadPmData(callback) {
+    fetch("https://docs.google.com/spreadsheets/d/1pE3ybRHT9OTXH0o7lwCi6naxziO3bm7P2V3d6SPiLzs/gviz/tq?tqx=out:json&gid=504311244")
+        .then(res => res.text())
+        .then(text => {
+            const json = JSON.parse(text.substring(47).slice(0, -2));
+            pmData = json.table.rows;
+            callback();
+        });
+}
+
+loadPmData(() => {
+    const values = pmData[1].c.map(cell => cell?.v || 0);
+    const data = {
+        labels: ['Manan Sharma (HCP)', 'Souptik Samanta (HCP)', 'Lynn (hUWUp)', 'Firepup Sixfifty (HGP)', 'Lucas (WME)', 'Manni Reis (HDP)', 'Navdeep Singh (HCRP)', 'Emma (:3)', 'Samannoy Bhattacharjee (HCLP)', 'Advait Contractor (Independent)', 'NonsensicalGibberish (Independent)', 'Loop (Independent)', 'Ryan (Independent)'],
+        datasets: [{
+            label: 'Vote share',
+            data: values,
+            backgroundColor: [
+                '#ff0000',
+                '#ff0000',
+                '#FF91AF',
+                '#9268F6',
+                '#00C4D6',
+                '#FCEBFF',
+                '#9585EE',
+                '#FFC8D6',
+                '#FCC624',
+                '#777777',
+                '#777777',
+                '#777777',
+                '#777777',
+            ],
+            borderColor: [
+                '#ffffff',
+                '#000000',
+                '#FF91AF',
+                '#9268F6',
+                '#00C4D6',
+                '#FCEBFF',
+                '#9585EE',
+                '#FFC8D6',
+                '#FCC624',
+                '#EDE621',
+                '#ffffff',
+                '#00C4D6',
+                '#FF91AF',
+            ],
+            borderWidth: 5,
+            rotation: 90
+        }]
+    };
+
+// config
+    const config = {
+        type: 'bar',
+        data: data,
+        skipNull: true,
+        options: {
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    };
+
+// render init block
+    new Chart(document.getElementById('chartPm'), config);
+
+// Instantly assign Chart.js version
+    const chartVersion = document.getElementById('chartVersion');
+    chartVersion.innerText = Chart.version;
+});
+
+let AttData;
+function loadAttData(callback) {
+    fetch("https://docs.google.com/spreadsheets/d/1pE3ybRHT9OTXH0o7lwCi6naxziO3bm7P2V3d6SPiLzs/gviz/tq?tqx=out:json&gid=1354773593")
+        .then(res => res.text())
+        .then(text => {
+            const json = JSON.parse(text.substring(47).slice(0, -2));
+            AttData = json.table.rows;
+            callback();
+        });
+}
+
+loadAttData(() => {
+    const values = AttData[1].c.map(cell => cell?.v || 0);
+    const data = {
+        labels: ['Lynn', 'Navdeep Singh'],
+        datasets: [{
+            label: 'Vote share',
+            data: values,
+            backgroundColor: [
+                '#FF91AF',
+                '#9585EE',
+            ],
+            borderColor: [
+                '#FF91AF',
+                '#9585EE',
+            ],
+            borderWidth: 5,
+            rotation: 90
+        }]
+    };
+
+// config
+    const config = {
+        type: 'bar',
+        data: data,
+        skipNull: true,
+        options: {
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    };
+
+// render init block
+    new Chart(document.getElementById('chartAtt'), config);
 
 // Instantly assign Chart.js version
     const chartVersion = document.getElementById('chartVersion');
